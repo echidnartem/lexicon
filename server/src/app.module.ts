e5a5467011d900d584e4e5d36e77782282d15260
Artyom Mitskevich <echidnartem@gmail.com>
2026-07-17 20:32:51 +0300

    Chore (server): remove unused WordsService provider

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,7 @@
 import { Module } from "@nestjs/common";
 import { AppController } from "./app.controller";
-import { WordsService } from './words/words.service';
 
 @Module({
   controllers: [AppController],
-  providers: [WordsService],
 })
 export class AppModule {}
